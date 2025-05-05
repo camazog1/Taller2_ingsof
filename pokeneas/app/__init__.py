1cdc7585ff68a36f3c45b4fb413a4078be1cea04
Pedro Saldarriaga <pedrosaldarriaga@MacBook-Air-de-Pedro.local>
2025-05-05 17:23:54 -0500

    ya ni se

--- pokeneas/app/__init__.py
+++ pokeneas/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .routes import register_routes
+import os
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config')
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    app = Flask(__name__, template_folder=template_dir)
+
+    app.config.from_object('app.config.Config')
+
     register_routes(app)
     return app

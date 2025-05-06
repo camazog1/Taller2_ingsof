7807cf1adad994248e9c31326a3a731d3017905f
camazog1 <camazog1@eafit.edu.co>
2025-05-06 15:49:14 -0500

    Git Action

--- pokeneas/app/config.py
+++ pokeneas/app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     # Nombre del bucket en S3 donde están las imágenes
-    S3_BUCKET = "nombre-de-tu-bucket"
+    S3_BUCKET = "pokenea-images"
 
     # Otros ejemplos de configuración (puedes agregarlos si los necesitas)
     DEBUG = False
